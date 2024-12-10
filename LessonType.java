12deac90036f57b09e6f00aabae27e7476c6f87d
ArturArystanbekov <156782799+ArturArystanbekov@users.noreply.github.com>
2024-12-11 02:36:20 +0500

    Update LessonType.java

--- LessonType.java
+++ LessonType.java
@@ -1,5 +1,15 @@
 package AdditionalClasses;
 
-public class LessonType {
+public enum LessonType {
+    LECTURE,
+    PRACTICE;
 
+    @Override
+    public String toString() {
+        switch (this) {
+            case LECTURE: return "Lecture";
+            case PRACTICE: return "Practice";
+            default: throw new IllegalArgumentException("Unknown lesson type");
+        }
+    }
 }
