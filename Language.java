59b96091ecf546c55c6b9fce2f9d7ceee7ab5477
ArturArystanbekov <156782799+ArturArystanbekov@users.noreply.github.com>
2024-12-11 02:35:55 +0500

    Update Language.java

--- Language.java
+++ Language.java
@@ -1,5 +1,18 @@
 package AdditionalClasses;
 
-public class Language {
 
+public enum Language {
+    KZ,
+    EN,
+    RU;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case KZ: return "Kazakh";
+            case EN: return "English";
+            case RU: return "Russian";
+            default: throw new IllegalArgumentException("Unknown language");
+        }
+    }
 }
